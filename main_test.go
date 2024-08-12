4c7d9516de7621930c84bd9cd9189fdb44520390
Philipp Nikitin <Nikitin.FS@yandex.ru>
2024-08-12 16:30:53 +0300

    refactor test functions

--- main_test.go
+++ main_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
 
@@ -10,8 +12,49 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// Функция urlForTest возвращает указатель на структуру типа url.URL,
+// сформированную на основе полученных аргументов и ошибку, если что-то пойдет
+// не так во время выполнения функции.
+
+// Была добавлена, чтобы не повторять один и тот же код в каждой тестовой функции
+// в соответствии с принципом DRY
+
+// Параметры
+// baseUrl - базовый URL, к которому будет добавлен эндпоинт и параметры запроса
+// endpoint - эндпоинт, который мы добавим к базовому URL
+// count - количество кафе, которое мы хотим получить
+// city - город, для которого мы запрашиваем информацию
+
+func urlForTest(baseUrl, endpoint, count, city string) (*url.URL, error) {
+	targetUrl, err := url.Parse(baseUrl)
+
+	// если возникла ошибка, возвращаем nil (нулевое значение для указателя) и ошибку
+	if err != nil {
+		return nil, err
+	}
+
+	// добавляем эндпоинт, который был передан в качестве аргумента
+	targetUrl.Path += endpoint
+
+	// Подготавливаем параметры запроса
+	params := url.Values{}
+	params.Add("count", count)
+	params.Add("city", city)
+
+	// добавляем параметры запроса к базовому URL при помощи params.Encode()
+	targetUrl.RawQuery = params.Encode()
+
+	return targetUrl, nil
+}
+
 func TestMainHandlerWhenOk(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
+	// создаем URL-адрес для запроса при помощи urlForTest
+	targetUrl, err := urlForTest("http://localhost:8080/", "cafe", "2", "moscow")
+	if err != nil {
+		fmt.Println("MalformedURL:", err)
+	}
+	// создаем новый запрос
+	req := httptest.NewRequest("GET", targetUrl.String(), nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
@@ -41,7 +84,13 @@ func TestMainHandlerWhenOk(t *testing.T) {
 }
 
 func TestMainHandlerWhenWrongCity(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=2&city=london", nil)
+	// создаем URL-адрес для запроса при помощи urlForTest
+	targetUrl, err := urlForTest("http://localhost:8080/", "cafe", "2", "london")
+	if err != nil {
+		fmt.Println("MalformedURL:", err)
+	}
+	// создаем новый запрос
+	req := httptest.NewRequest("GET", targetUrl.String(), nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
@@ -55,7 +104,13 @@ func TestMainHandlerWhenWrongCity(t *testing.T) {
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4 // общее количество кафе
-	req := httptest.NewRequest("GET", "/cafe?count=5&city=moscow", nil)
+	// создаем URL-адрес для запроса при помощи urlForTest
+	targetUrl, err := urlForTest("http://localhost:8080/", "cafe", "7", "moscow")
+	if err != nil {
+		fmt.Println("MalformedURL:", err)
+	}
+	// создаем новый запрос
+	req := httptest.NewRequest("GET", targetUrl.String(), nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
@@ -67,7 +122,7 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	// проверяем, что вернулся код ответа 200
 	assert.Equal(t, http.StatusOK, responseRecorder.Code)
 	// также хотел вместо assert.Equal использовать метод пакета assert HTTPSuccess:
-	// assert.HTTPSuccess(t, handler, "GET", "/cafe?count=5&city=moscow", nil)
+	// assert.HTTPSuccess(t, handler, "GET", "/cafe?count=7&city=moscow", nil)
 	// но тест возврашал значение FAIL - получался код ответа 400, а не 200
 	assert.Equal(t, totalCount, len(cafes))
 	// кроме длина слайса кафе, также можно проверить ожидаемый ответ через
